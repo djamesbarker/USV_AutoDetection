f52c08988638498b18a08ad5212aec5e7f35bce8
djamesbarker <djamesbarker@gmail.com>
2014-09-15 12:02:16 -0400

    Stream2Excel
    Introduction of Stream2Excel tool which allows users to process multiple
    files as a stream and subsequently export excel files of the detections
    for each individual file in the strea,

--- Extensions/Actions/Log/Stream2Excel/private/compute.m
+++ Extensions/Actions/Log/Stream2Excel/private/compute.m
@@ -1,10 +1,8 @@
 function [result, context] = compute(log, parameter, context)
+%Stream2Excel - Compute:
+%Takes a log from a filestream and makes an excel file for each sound
+%within the Stream log
 
-%Stream2Raven - Compute:
-%Takes a log from a filestream and makes Raven compatible selection
-%tables (.txt files) for each sound file within the log
-
-%% Add sound files for each event into every log.event structure - uses code from 'Get Files' Log Action
 if ischar(log.sound.file) || iscellstr(log.sound.file) == 1 %Make sure the name of the file is a string
     [starttime duration] = get_file_times(log.sound); %Function that outputs start time of each sound and its duration
     x = length(log.sound.file);
@@ -35,18 +33,19 @@ result = log;
 % (Only certain fields really need to be changed i.e. event, sound, length, curr_id)
 
 cd(log.path(1:end-5));
-mkdir('Raven Logs');
-Rav_dir = [log.path(1:end-5) 'Raven Logs'];
-
+mkdir('Output Logs');
+Rav_dir = [log.path(1:end-5) 'Output Logs'];
 
 for i=1:length(result.sound.file)
     [~,file_name]=fileparts(result.sound.file{i});
-    Rav_name=[file_name '.txt'];
+    Rav_name=[file_name];
     
 write_data = {}; write_data{1} = 'Selection';
 write_data{2} = 'View'; write_data{3} = 'Channel';
 write_data{4} = 'Begin Time (s)'; write_data{5} = 'End Time (s)';
 write_data{6} = 'Low Freq (Hz)'; write_data{7} = 'High Freq (Hz)';
+write_data{8} = 'Peak Freq (Hz)';write_data{9} = 'Peak Time (Hz)';
+    
     
     %Rav_name = [result.sound.file{i}(1:end-8) '.txt'];
     cd(log.path);
@@ -79,15 +78,26 @@ write_data{6} = 'Low Freq (Hz)'; write_data{7} = 'High Freq (Hz)';
         write_data{a+1,2} = 'Spectrogram 1'; write_data{a+1,3} = 1;
         write_data{a+1,4} = tmp_log.event(a).time(1); write_data{a+1,5} = tmp_log.event(a).time(2);
         write_data{a+1,6} = tmp_log.event(a).freq(1); write_data{a+1,7} = tmp_log.event(a).freq(2);
+        write_data{a+1,8} = tmp_log.event(a).peakHz;
+        write_data{a+1,9} = tmp_log.event(a).peakTime;
+        
     end
     
     cd(Rav_dir);
-    dlmwrite(Rav_name,'');  %Create text file to write to
-    Rav_ID = fopen(Rav_name,'w');   %Open the text file we just made
-    fprintf(Rav_ID,'%s \t',write_data{1,1},write_data{1,2},write_data{1,3},write_data{1,4},write_data{1,5},write_data{1,6});
-    fprintf(Rav_ID,'%s \n',write_data{1,7});
-    for b = 2:(length(tmp_log.event)+1)
-        fprintf(Rav_ID,'%1.0f\t %s\t %1.0f\t %3.21f\t %3.21f\t %5.23f\t %5.23f\n',...
-            write_data{b,1},write_data{b,2},write_data{b,3},write_data{b,4},write_data{b,5},write_data{b,6},write_data{b,7});
-    end
+    xlswrite([Rav_name '.xls'],write_data)
+%% Original Stream2Raven Output Code   
+%     dlmwrite(Rav_name,'');  %Create text file to write to
+%     Rav_ID = fopen(Rav_name,'w');   %Open the text file we just made
+%     fprintf(Rav_ID,'%s \t',write_data{1,1},write_data{1,2},write_data{1,3},write_data{1,4},write_data{1,5},write_data{1,6});
+%     fprintf(Rav_ID,'%s \n',write_data{1,7});
+%     for b = 2:(length(tmp_log.event)+1)
+%         fprintf(Rav_ID,'%1.0f\t %s\t %1.0f\t %3.21f\t %3.21f\t %5.23f\t %5.23f\n',...
+%             write_data{b,1},write_data{b,2},write_data{b,3},write_data{b,4},write_data{b,5},write_data{b,6},write_data{b,7});
+%     end
 end
+
+    
+
+    
+end
+
---
+++ Extensions/Actions/Log/Stream2Excel/stream2excel.m
@@ -0,0 +1,16 @@
+function ext = stream2excel
+
+ext = extension_create;
+
+ext.short_description = 'Creates individual Excel output for each file in stream';
+
+% ext.category = {};
+
+ext.version = '1.0';
+
+ext.author = 'Default';
+
+ext.email = '';
+
+ext.url = '';
+
